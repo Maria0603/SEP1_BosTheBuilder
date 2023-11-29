b0bec24c916b68943737847ce89893b9637858f7
AleksandraVIA <293687@via.dk>
2023-11-29 14:52:37 +0100

    Main changes
    asd

--- application/appSEP/src/Main.java
+++ application/appSEP/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!11112222");
     System.out.println("github desktop");
+    System.out.println("github desktop2");
   }
 }
